8e0217fa1652b2788d69fb9ec12a7a6e82ffdb76
Akilgour <kilgourkilgour@hotmail.com>
2015-05-24 22:54:24 +0100

    Added GamePlaySetting controler and put in a basic index page

---
+++ ForbiddenIslandMVCTwo/Controllers/GamePlaySettingController.cs
@@ -0,0 +1,112 @@
+﻿using ForbiddenIslandMVCTwo.Context;
+using ForbiddenIslandMVCTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForbiddenIslandMVCTwo.Controllers
+{
+    public class GamePlaySettingController : Controller
+    {
+        //
+        // GET: /GamePlaySetting/
+
+        public ActionResult Index()        
+        {
+            using (var context = new ForbiddenIslandContext())
+            {
+                var query = from gps in context.GamePlaySettings
+                            select gps;
+                return View(query.ToList());
+            }         
+        }
+
+        //
+        // GET: /GamePlaySetting/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /GamePlaySetting/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /GamePlaySetting/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /GamePlaySetting/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /GamePlaySetting/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /GamePlaySetting/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /GamePlaySetting/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
