b274590de4b8eae9ec107e02ae2049f3edcda26a
Akilgour <kilgourkilgour@hotmail.com>
2015-06-19 21:34:52 +0100

    Created island of tile viewmodel controller and view

---
+++ ForbiddenIslandMVCTwo/Controllers/IslandOfTilesController.cs
@@ -0,0 +1,31 @@
+﻿using ForbiddenIslandMVCTwo.Context;
+using ForbiddenIslandMVCTwo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForbiddenIslandMVCTwo.Controllers
+{
+    public class IslandOfTilesController : Controller
+    {
+        //
+        // GET: /Island/
+        public ActionResult Index(Guid gameId)
+        {
+            using (var context = new ForbiddenIslandContext())
+            {
+                var query = context.IslandTiles.Where(x => x.GameId == gameId);
+                var islandOfTiles = new IslandOfTiles();
+
+                //TODO should be factory, but just test stuff
+                islandOfTiles.AllIslandTile = query.ToList();
+
+                return View(islandOfTiles);
+
+            }
+        }
+
+    }
+}
---
+++ ForbiddenIslandMVCTwo/ViewModels/IslandOfTiles.cs
@@ -0,0 +1,69 @@
+﻿using ForbiddenIslandMVCTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForbiddenIslandMVCTwo.ViewModels
+{
+    public class IslandOfTiles
+    {
+
+        public List<IslandTile> AllIslandTile { get; set; }
+
+        public List<IslandTile> FirstRow
+        {
+            get
+            
+            {
+                return GetItemsAtRow(1);
+            }
+        }
+
+        public List<IslandTile> SecondRow
+        {
+            get
+            {
+                return GetItemsAtRow(2);
+            }
+        }
+
+        public List<IslandTile> ThirdRow
+        {
+            get
+            {
+                return GetItemsAtRow(3);
+            }
+        }
+
+        public List<IslandTile> FourthRow
+        {
+            get
+            {
+                return GetItemsAtRow(4);
+            }
+        }
+
+        public List<IslandTile> FifthRow
+        {
+            get
+            {
+                return GetItemsAtRow(5);
+            }
+        }
+
+        public List<IslandTile> SixthRow
+        {
+            get
+            {
+                return GetItemsAtRow(6);
+            }
+        }
+
+        private List<IslandTile> GetItemsAtRow(int index)
+        {
+            return AllIslandTile.Where(x => x.RowNumber == index).OrderBy(x => x.ColumnNumber).ToList();
+        }
+
+    }
+}
