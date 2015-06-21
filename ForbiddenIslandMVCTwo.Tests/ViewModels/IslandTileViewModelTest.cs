b52357b40fb9025b0542f6e4a18a2e08e5fa97c8
Akilgour <kilgourkilgour@hotmail.com>
2015-06-21 22:33:09 +0100

    Upade the islandtileviewmodel  so filename is based on the islandtile name

---
+++ ForbiddenIslandMVCTwo.Tests/ViewModels/IslandTileViewModelTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForbiddenIslandMVCTwo.ViewModels;
+ 
+
+namespace ForbiddenIslandMVCTwo.Tests.ViewModels
+{
+    //[TestClass]
+    //public class IslandTileViewModelTest
+    //{
+    //    string noTile = @"~/Images/120x120/No_Tile.jpg";
+    //    string phantomRock = @"~/Images/120x120/Phantom_Rock.jpg";
+    //    string bronzeGate = @"~/Images/120x120/Bronze_Gate.jpg";
+
+        
+
+    //    [TestMethod]
+    //    public void FileName_Empty()
+    //    {
+    //        //Arrange 
+    //        var islandTile = new IslandTileViewModel();
+    //        islandTile.Name = "";
+           
+    //        //Act 
+    //        var filePath = islandTile.FilePath;
+
+    //        //Assert
+    //        Assert.AreEqual(filePath, noTile);
+    //    }
+
+    //    [TestMethod]
+    //    public void FileName_NoFileName()
+    //    {
+    //        //Arrange 
+    //        var islandTile = new IslandTileViewModel();
+    //        islandTile.Name = "NoFileName";
+
+    //        //Act 
+    //        var filePath = islandTile.FilePath;
+
+    //        //Assert
+    //        Assert.AreEqual(filePath, noTile);
+    //    }
+
+    //    [TestMethod]
+    //    public void FileName_BronzeGate()
+    //    {
+    //        //Arrange 
+    //        var islandTile = new IslandTileViewModel();
+    //        islandTile.Name = "Bronze Gate";
+
+    //        //Act 
+    //        var filePath = islandTile.FilePath;
+
+    //        //Assert
+    //        Assert.AreEqual(filePath, bronzeGate);
+    //    }
+
+    //    [TestMethod]
+    //    public void FileName_PhantomRock()
+    //    {
+    //        //Arrange 
+    //        var islandTile = new IslandTileViewModel();
+    //        islandTile.Name = "Phantom Rock";
+
+    //        //Act 
+    //        var filePath = islandTile.FilePath;
+
+    //        //Assert
+    //        Assert.AreEqual(filePath, phantomRock);
+    //    }
+
+    //    [TestMethod]
+    //    public void FileName_Phantom_Rock()
+    //    {
+
+    //        using (ShimsContext.Create())
+    //        {
+    //            // Arrange:
+    //            // Shim DateTime.Now to return a fixed date:
+    //            System.Fakes.ShimDateTime.NowGet =
+    //            () =>
+    //            { return new DateTime(fixedYear, 1, 1); };
+
+
+    //            //Arrange 
+    //            var islandTile = new IslandTileViewModel();
+    //            islandTile.Name = "Phantom_Rock";
+
+    //            //Act 
+    //            var filePath = islandTile.FilePath;
+
+    //            //Assert
+    //            Assert.AreEqual(filePath, phantomRock);
+
+    //        }
+    //    }
+    //}
+}
--- ForbiddenIslandMVCTwo/ViewModels/IslandTileViewModel.cs
+++ ForbiddenIslandMVCTwo/ViewModels/IslandTileViewModel.cs
@@ -14,14 +14,32 @@ namespace ForbiddenIslandMVCTwo.ViewModels
         public int RowNumber { get; set; }
         public int ColumnNumber { get; set; }
 
+        /// <summary>
+        /// Will get the filePath of the of image based on its name
+        /// It will replace all spaces in the name with underscores
+        /// If there is no file of that name it will return the ugly no tile image
+        /// </summary>
         public string FilePath
         {
             get
             {
-                return @"~/Images/120x120/Phantom_Rock.jpg";
+                if (!string.IsNullOrEmpty(Name))
+                {
+                    string fileWithFullPath = String.Format(@"~/Images/120x120/{0}.{1}", Name.Replace(" ", "_"), "jpg");
+                    if (File.Exists(HttpContext.Current.Server.MapPath(fileWithFullPath)))
+                    {
+                        return fileWithFullPath;
+                    }
+                }
+                return @"~/Images/120x120/No_Tile.jpg";
             }
         }
 
+        /// <summary>
+        /// This will display a image
+        /// Can be used with : 
+        /// <img src="data:image;base64,@System.Convert.ToBase64String(Model.ImageArray)" />
+        /// </summary>
         public byte[] ImageArray
         {
             get
@@ -29,8 +47,7 @@ namespace ForbiddenIslandMVCTwo.ViewModels
                 //edit img https://msdn.microsoft.com/en-us/library/4sahykhd%28v=vs.110%29.aspx
                 using (var memoryStream = new MemoryStream())
                 {
-                    var imagePath = FilePath;
-                    String path = HttpContext.Current.Server.MapPath(imagePath);
+                    String path = HttpContext.Current.Server.MapPath(FilePath);
                     var image = Image.FromFile(path);
                     image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Gif);
                     return memoryStream.ToArray();
