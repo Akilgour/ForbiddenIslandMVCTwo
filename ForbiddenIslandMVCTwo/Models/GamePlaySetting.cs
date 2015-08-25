9b05c5ea2d899769ffcd6b0ec2e1559eb220ae27
Akilgour <kilgourkilgour@hotmail.com>
2015-08-25 22:16:12 +0100

    Cleaned up the style of gameplay settings

--- ForbiddenIslandMVCTwo/Models/GamePlaySetting.cs
+++ ForbiddenIslandMVCTwo/Models/GamePlaySetting.cs
@@ -18,17 +18,23 @@ namespace ForbiddenIslandMVCTwo.Models
         public Guid? SecondMovePlayerId { get; set; }
         public Guid? ThirdMovePlayerId { get; set; }
         public Guid? FourthMovePlayerId { get; set; }
+        [Display(Name = "Move Number")]
         public int MoveNumber { get; set; }
+        [Display(Name = "Water Level")]
         public int WaterLevelMarker { get; set; }
         public Guid DrawDeckId { get; set; }
         public Guid DiscardDeckId { get; set; }
 
+        [Display(Name = "First Move Player")]
         [ForeignKey("FirstMovePlayerId")]
         public virtual Player FirstMovePlayer { get; set; }
+        [Display(Name = "Second Move Player")]
         [ForeignKey("SecondMovePlayerId")]
         public virtual Player SecondMovePlayer { get; set; }
+        [Display(Name = "Third Move Player")]
         [ForeignKey("ThirdMovePlayerId")]
         public virtual Player ThirdMovePlayer { get; set; }
+        [Display(Name = "Four Move Player")]
         [ForeignKey("FourthMovePlayerId")]
         public virtual Player FourthMovePlayer { get; set; }
         public virtual List<TreasureCard> DrawDeck { get; set; }
