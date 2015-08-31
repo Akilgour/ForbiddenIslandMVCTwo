5d868ac28c0a067710d3fa1468f78b44199274a0
Akilgour <kilgourkilgour@hotmail.com>
2015-08-31 23:31:30 +0100

    Added Dependency Resolver project

---
+++ DependencyResolver/DR.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyResolver
+{
+    public class DR
+    {
+
+        public DR()
+        {
+            Items = new List<DRItem>();
+        }
+
+        public List<DRItem> Items { get; set; }
+
+
+        public void Register<T>(string ClassName, string CountryName)
+        {
+            var item = new DRItem(typeof(T), ClassName, CountryName);
+            Items.Add(item);
+        }
+
+        public void Register<T>(string ClassName)
+        {
+            var item = new DRItem(typeof(T), ClassName);
+            Items.Add(item);
+        }
+
+
+        public IEnumerable<T> GetList<T>(string aaa)
+        {
+            var types = GetAllClassesOfType(typeof(T), aaa);
+            return from item in types
+                   select (T)Activator.CreateInstance(item);
+        }
+
+        private List<Type> GetAllClassesOfType(Type type, string aaa)
+        {
+
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => type.IsAssignableFrom(p) && p.IsClass && p.IsPublic && !p.IsGenericType && p.Name == aaa).ToList();
+            return types;
+        }
+
+
+        public IEnumerable<T> GetList<T>()
+        {
+            var types = GetAllClassesOfType(typeof(T));
+            return from item in types
+                   select (T)Activator.CreateInstance(item);
+        }
+
+        private List<Type> GetAllClassesOfType(Type type)
+        {
+
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => type.IsAssignableFrom(p) && p.IsClass && p.IsPublic && !p.IsGenericType).ToList();
+            return types;
+        }
+
+        public T GetSingleByClassName<T>(string className)
+        {
+            var types = GetSingleByClassName(typeof(T), className);
+            return (T)Activator.CreateInstance(types);
+        }
+
+        public T GetSingleByClassName<T>()
+        {
+            var className = Items.Where(x => x.Interface.Name == typeof(T).Name).Single().ClassName;
+
+            var types = GetSingleByClassName(typeof(T), className);
+            return (T)Activator.CreateInstance(types);
+        }
+
+        private Type GetSingleByClassName(Type type, string aaa)
+        {
+
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+    .SelectMany(s => s.GetTypes())
+    .Where(p => type.IsAssignableFrom(p) && p.IsClass && p.IsPublic && !p.IsGenericType && p.Name == aaa);
+            return types.First();
+        }
+    }
+}
+
---
+++ DependencyResolver/DRItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyResolver
+{
+    public class DRItem
+    {
+        public DRItem()
+        {
+
+        }
+
+        public DRItem(Type Interface, string ClassName)
+            : this(Interface, ClassName, string.Empty)
+        {
+        }
+
+        public DRItem(Type Interface, string ClassName, string Country)
+        {
+            this.Interface = Interface;
+            this.ClassName = ClassName;
+        }
+
+        public Type Interface { get; private set; }
+
+        public string ClassName { get; private set; }
+    }
+}
