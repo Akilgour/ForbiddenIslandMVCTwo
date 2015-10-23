30d8d4b816db0b6b61fd80200e82f312e4ee713c
Akilgour <kilgourkilgour@hotmail.com>
2015-10-23 01:32:01 +0100

    Added Parameterized CanMove_RowOneSpaceTest

---
+++ NUnitTest/Validation/CanMove/CanMove_RowOneSpaceTest.cs
@@ -0,0 +1,36 @@
+﻿using ForbiddenIslandMVCTwo.Constants;
+using ForbiddenIslandMVCTwo.Models;
+using ForbiddenIslandMVCTwo.Validation.CanMove;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest.Validation.CanMove
+{
+    [TestFixture]
+    public class CanMove_RowOneSpaceTest
+    {
+        [TestCase(1, 3, 2, 3, true, null)]// Move OneRow RowOne To RowTwo IsValid
+        [TestCase(2, 3, 1, 3, true, null)]// Move OneRow RowTwo To RowOne IsValid
+        [TestCase(1, 3, 3, 3, false, CanMoveErrorConstants.ONLY_MOVE_ONE_ROW)]// Move TwoRow RowOne To RowThree NotValid
+        [TestCase(3, 3, 1, 3, false, CanMoveErrorConstants.ONLY_MOVE_ONE_ROW)]// Move TwoRow RowThree To RowOne NotValid
+        public void MoveOneRowRowOneToRowTwo_IsValid(int firstTileRowNumber, int firstTileColumnNumber, int secondTileRowNumber, int secondTileColumnNumber, bool isValid, string errorMessage)
+        {
+            //Arrange 
+            var canMove = new CanMove_RowOneSpace();
+            var firstTile = new IslandTile() { RowNumber = firstTileRowNumber, ColumnNumber = firstTileColumnNumber };
+            var secondTile = new IslandTile() { RowNumber = secondTileRowNumber, ColumnNumber = secondTileColumnNumber };
+            var currentPlayer = new Player();
+
+            //Act 
+            var validation = canMove.IsValid(firstTile, secondTile, currentPlayer);
+
+            //Assert
+            Assert.AreEqual(isValid, validation.IsValid);
+            Assert.AreEqual(errorMessage, validation.ErrorMessage);
+        }
+    }
+}
