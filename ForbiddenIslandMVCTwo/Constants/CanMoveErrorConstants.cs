ce0bf66bbd34574bdd54659c3f4ff280adcd28f7
Akilgour <kilgourkilgour@hotmail.com>
2015-10-23 01:08:14 +0100

    Added Parameterized CanMove_ColumnOneSpaceTest

--- ForbiddenIslandMVCTwo/Constants/CanMoveErrorConstants.cs
+++ ForbiddenIslandMVCTwo/Constants/CanMoveErrorConstants.cs
@@ -7,10 +7,10 @@ namespace ForbiddenIslandMVCTwo.Constants
 {
     public static class CanMoveErrorConstants
     {
-        public static string ONLY_MOVE_ONE_ROW = "Can only move one row at a time";
-        public static string ONLY_MOVE_ONE_COLUMN = "Can only move one column at a time";
-        public static string NEED_FOUR_MOVE_TILES = "MoveType four player move tiles";
-        public static string CANMOVEVALIDATION_CANNOT_BE_NULL = "CanMoveValidation cannot be null";
-        public static string PLAYERMOVES_CANNOT_BE_NULL = "PlayerMoves cannot be null";
+        public const string ONLY_MOVE_ONE_ROW = "Can only move one row at a time";
+        public const string ONLY_MOVE_ONE_COLUMN = "Can only move one column at a time";
+        public const string NEED_FOUR_MOVE_TILES = "MoveType four player move tiles";
+        public const string CANMOVEVALIDATION_CANNOT_BE_NULL = "CanMoveValidation cannot be null";
+        public const string PLAYERMOVES_CANNOT_BE_NULL = "PlayerMoves cannot be null";
     }
 }
---
+++ NUnitTest/Validation/CanMove/CanMove_ColumnOneSpaceTest.cs
@@ -0,0 +1,37 @@
+﻿using ForbiddenIslandMVCTwo.Constants;
+using ForbiddenIslandMVCTwo.Models;
+using ForbiddenIslandMVCTwo.Validation.CanMove;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest.Validation.CanMove
+{
+    [TestFixture]
+    public class CanMove_ColumnOneSpaceTest
+    {
+        [TestCase(3, 1, 3, 2, true, null)]// Move OneRow ColumnOne To ColumnTwo IsValid
+        [TestCase(3, 2, 3, 1, true, null)] // Move OneRow ColumnTwo To ColumnOne IsValid
+        [TestCase(3, 1, 3, 3, false, CanMoveErrorConstants.ONLY_MOVE_ONE_COLUMN)]// Move TwoRows ColumnOne To ColumnThree NotValid
+        [TestCase(3, 3, 3, 1, false, CanMoveErrorConstants.ONLY_MOVE_ONE_COLUMN)]// Move TwoRows ColumnThree To ColumnOne NotValid
+        public void MoveOneRowColumnOneToColumnTwo_IsValid(int firstTileRowNumber, int firstTileColumnNumber, int secondTileRowNumber, int secondTileColumnNumber, bool isValid, string errorMessage)
+        {
+            //Arrange 
+            var canMove = new CanMove_ColumnOneSpace();
+            var firstTile = new IslandTile() { RowNumber = firstTileRowNumber, ColumnNumber = firstTileColumnNumber };
+            var secondTile = new IslandTile() { RowNumber = secondTileRowNumber, ColumnNumber = secondTileColumnNumber };
+            var currentPlayer = new Player();
+
+            //Act 
+            var validation = canMove.IsValid(firstTile, secondTile, currentPlayer);
+
+            //Assert
+            Assert.AreEqual(isValid, validation.IsValid);
+            Assert.AreEqual(errorMessage, validation.ErrorMessage);
+        }
+
+    }
+}
